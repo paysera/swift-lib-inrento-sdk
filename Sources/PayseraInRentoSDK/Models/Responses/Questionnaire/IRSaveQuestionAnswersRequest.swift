9fe3dfc7dc81ac61a3a45227b10a97798a57944a
Gerald Adorza <gerald.adorza@paysera.net>
2022-05-06 23:51:53 +0800

    Fixed mapping

--- Sources/PayseraInRentoSDK/Models/Responses/Questionnaire/IRSaveQuestionAnswersRequest.swift
+++ Sources/PayseraInRentoSDK/Models/Responses/Questionnaire/IRSaveQuestionAnswersRequest.swift
@@ -1,37 +1,44 @@
-public enum IRQuestionAnswerType {
-    case single(String)
-    case array([String])
+public struct AnsweredQuestion {
+    public let key: String
+    public let input: Input
     
-    var value: Any {
-        switch self {
-        case .single(let data):
-            return data
-        case .array(let data):
-            return data
+    public init(
+        key: String,
+        input: Input
+    ) {
+        self.key = key
+        self.input = input
+    }
+}
+
+extension AnsweredQuestion {
+    public enum Input {
+        case single(String)
+        case array([String])
+        
+        fileprivate var value: Any {
+            switch self {
+            case .single(let data):
+                return data
+            case .array(let data):
+                return data
+            }
         }
     }
 }
 
 public struct IRSaveQuestionAnswersRequest {
-    public let answers: [IRQuestionAnswerType]
+    public let answers: [AnsweredQuestion]
     
-    public init(answers: [IRQuestionAnswerType]) {
+    public init(answers: [AnsweredQuestion]) {
         self.answers = answers
     }
     
     public func toJSON() -> [String: Any] {
-        guard !answers.isEmpty else {
-            return [:]
+        answers.reduce([:]) { partialResult, answer in
+            var result = partialResult
+            result[answer.key] = answer.input.value
+            return result
         }
-        
-        let maxQuestions = min(answers.count, 12)
-        var json: [String: Any] = [:]
-        for idx in 0..<maxQuestions {
-            let isMax = idx == maxQuestions - 1
-            let suffix = isMax ? "_more" : ""
-            let formattedIndex = isMax ? idx : idx + 1
-            json["question\(formattedIndex)\(suffix)"] = answers[idx].value
-        }
-        return json
     }
 }
