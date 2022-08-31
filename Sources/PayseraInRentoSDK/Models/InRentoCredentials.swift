c72837bb7ac70856d5f0bdbbc28d10c0db441e03
Moh3n <khosravinia.mohsen@gmail.com>
2022-08-31 09:54:46 +0430

    App 3969 - Implement relate API (#6)
    * Update credential model.
    * Remove refreshTokenIssuedAt property from credentials.
    * Remove an extra colon.

--- Sources/PayseraInRentoSDK/Models/InRentoCredentials.swift
+++ Sources/PayseraInRentoSDK/Models/InRentoCredentials.swift
@@ -4,15 +4,21 @@ final public class InRentoCredentials {
     public var accessToken: String?
     public var accessTokenExpiresAt: Date?
     public var accessTokenIssuedAt: Date?
+    public var refreshToken: String?
+    public var refreshTokenExpiresAt: Date?
     
     public init(
         accessToken: String? = nil,
         accessTokenExpiresAt: Date? = nil,
-        accessTokenIssuedAt: Date? = nil
+        accessTokenIssuedAt: Date? = nil,
+        refreshToken: String? = nil,
+        refreshTokenExpiresAt: Date? = nil
     ) {
         self.accessToken = accessToken
         self.accessTokenExpiresAt = accessTokenExpiresAt
         self.accessTokenIssuedAt = accessTokenIssuedAt
+        self.refreshToken = refreshToken
+        self.refreshTokenExpiresAt = refreshTokenExpiresAt
     }
     
     public func hasExpired() -> Bool {
