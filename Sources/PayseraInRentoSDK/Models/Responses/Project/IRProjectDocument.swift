93206d5de35811966160e9c444c5294a13e397ca
Moh3n <khosravinia.mohsen@gmail.com>
2022-12-17 16:29:15 +0330

    APP-4267: Inrento: Incorrect project document on iOS (#10)

--- Sources/PayseraInRentoSDK/Models/Responses/Project/IRProjectDocument.swift
+++ Sources/PayseraInRentoSDK/Models/Responses/Project/IRProjectDocument.swift
@@ -3,11 +3,13 @@ public struct IRProjectDocument: Decodable {
     public let url: String
     public let categoryId: Int?
     public let categoryName: String?
+    public let language: String?
     
     private enum CodingKeys: String, CodingKey {
         case name
         case url
         case categoryId = "category_id"
         case categoryName = "category_name"
+        case language
     }
 }
